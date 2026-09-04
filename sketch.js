4774a79625b7d918ca5d0753f0814913c9d878ee
Policron <pwma08@gmail.com>
2026-09-04 15:00:12 -0700

    ammo and difficulty+graphics

--- sketch.js
+++ sketch.js
@@ -1,22 +1,18 @@
 // Project Title
-// Your Name Pilicron
+// Your Name Policron
 // Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
 
 // Declare global variables
-let isAlive = true; // allows the duck to be shot
-let up = 0; // To help make the wings flap
-//TODO #3 Declare and initialize a duckX variable
+let isAlive = true;
+let up1 = 0;
 let duckX = 300
-//TODO #3 Declare and initialize a duckY variable
+let duckXSpeed = 5
 let duckY = 200
-// TODO #7 Implement a score system
+let duckYSpeed = 5
 let scoreValue = 0
-// TODO #8 Make the difficulty increase as the score increases
-// TODO #9+ Sound? Reload? Dog? More ducks?
+let difficulty = 0
+let ammo = 3
+let lose = false
 
 function setup() {
   createCanvas(600, 400);
@@ -34,83 +30,95 @@ function drawDuck(x, y) {
   // duck head
   fill("darkgreen");
   ellipse(x + 20, y - 10, 10, 10);
-  // duck wing
+  // duck wings
   fill("lightgrey");
-  arc(x + 5, y, 10, 40, (-1 * 3.14159) / 8 + up, 3.14159 + up);
-  // duck beak and feet
+  arc(x + 5, y, 10, 40, (-1 * 3.14159) / 8 + up1, 3.14159 + up1);
+  // duck feet & beak
   fill("orange");
   ellipse(x + 26, y - 6, 8, 4);
   ellipse(x - 13, y + 12, 6, 6);
   ellipse(x - 8, y + 12, 6, 6);
-  // increment flap for wing movement
   if(isAlive) {
-  up += PI;
+  up1 += PI;
   }
 }
 
 function mousePressed() {
-  if (dist (mouseX, mouseY, duckX, duckY) <=30) {
+  if (dist (mouseX, mouseY, duckX, duckY) <=30&& isAlive == true) {
     isAlive = false
     scoreValue += 10
     background("red")
+    ammo -= 1
   }
   else {
     background("white")
     scoreValue -= 10
+    ammo -= 1
   }
-  // TODO #6 Write conditional statement or statements
-  // that represents whether the mouse position
-  // is over the duck body.
-  // You can nest if statements or use the && operator
 }
 
 function draw() {
-  background(235); //redraw the background
-
-  //TODO #1 call the drawDuck function
+  background("lightblue");
   drawDuck(duckX, duckY)
-
-  // TODO #2 Draw crosshairs around the mouse
-  //    (two crossing lines and a circle around)
   drawCrosshairs();
-  // Code to move the duck
-  // TODO #4 Write a Conditional statement so
-  // that if the duck is alive, it "Flies"
-  //                - moves up and across
   moveDuck()
-  // TODO #4 Otherwise the duck should move
-  // straight down only
-  // Also assign 'up' variable the value of 0
-  // this will make the wings stop flapping
-  // TODO #5 Make the duck re-appear
-  // Write a conditional statement so that
-  //   if the duck is too high or too low (off canvas),
-  //   it respawns to the bottom of the canvas
-  text ("score", 10, 390)
+  if (scoreValue >= 50) {
+    drawDuck(duckX-50, duckY)
+  }
+  duckX += duckXSpeed
+  duckY += duckYSpeed
+  fill (0, 200, 0)
+  noStroke()
+  rect (0, 330, 600, 70)
+  fill(0)
+  text ("score", 10, 390)     //score text
   text (scoreValue, 50, 390)
+  text ("difficulty", 500, 390) //difficulty text
+  text (difficulty, 550, 390)
+  text ("ammo", 200, 390) //ammo text
+  text (ammo, 250, 390)
+
+  if (scoreValue >= 250) {
+    difficulty = 8
+  }
+  else if (scoreValue >= 200) {
+    difficulty = 6
+  }
+  else if (scoreValue >= 150) {
+    difficulty = 4
+  }
+  else if (scoreValue >= 100) {
+    difficulty = 2
+  }
 }
 
 function drawCrosshairs() {
   noFill();
-  stroke(255, 0, 0)
+  stroke(0);
   circle(mouseX, mouseY, 40);
   line(mouseX - 20, mouseY, mouseX + 20, mouseY);
   line(mouseX, mouseY - 20, mouseX, mouseY + 20)
-  stroke(0)
 }
 
 function moveDuck() {
-  if (isAlive == true) { //flies
-    duckX += 5
-    duckY -= 5
+  if (isAlive == true) {
+    duckXSpeed= 4 + difficulty
+    duckYSpeed = -4 - difficulty
   }
   else if (isAlive == false) {
     up = 0
-    duckY += 5
+    duckXSpeed = 0
+    duckYSpeed = 10
   }
   if (duckY <= -50 || duckY >= 450 || duckX <= -50 || duckX >= 650) {
     duckX = random (100,300)
     duckY = 450
     isAlive = true
   }
+}
+function cloud(x, y) {
+  fill("white");
+  ellipse(x, y, 30, 20);
+  ellipse(x + 15, y - 10, 20, 15);
+  ellipse(x - 15, y - 10, 20, 15);
 }
